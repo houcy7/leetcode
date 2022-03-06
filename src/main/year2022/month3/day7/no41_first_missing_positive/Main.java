4180dbad823a97158bf210969b1632ae7c90a6d4
houcy7 <1450166510@qq.com>
2022-03-06 23:25:14 +0800

    a

---
+++ src/main/year2022/month3/day7/no41_first_missing_positive/Main.java
@@ -0,0 +1,8 @@
+package year2022.month3.day7.no41_first_missing_positive;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+    }
+}
---
+++ src/main/year2022/month3/day7/no41_first_missing_positive/Solution.java
@@ -0,0 +1,5 @@
+package year2022.month3.day7.no41_first_missing_positive;
+
+public class Solution {
+
+}
